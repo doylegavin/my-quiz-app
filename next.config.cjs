367550bd5760d18428a78a877866ca18f4d73a14
Gavin Doyle <doyle.d.gavin@gmail.com>
2025-05-12 10:25:00 +0100

    added feedback button

--- next.config.cjs
+++ next.config.cjs
@@ -26,7 +26,25 @@ const nextConfig = {
     'next-auth': {
       transform: 'next-auth/{{path}}'
     }
-  }
-}
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://eu-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://eu.i.posthog.com/:path*',
+      },
+      {
+        source: '/ingest/decide',
+        destination: 'https://eu.i.posthog.com/decide',
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
+};
 
 module.exports = nextConfig;
